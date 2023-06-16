67fec0dc4420d0f473c09358c7d1485246c1f079
wellerson <wellersonjava@gmail.com>
2023-06-16 18:30:01 -0300

    Adicionar aquivos do jogo da snake

---
+++ src/game/GameFrame.java
@@ -0,0 +1,17 @@
+package game;
+
+import javax.swing.JFrame;
+import java.awt.*;
+
+public class GameFrame extends JFrame{
+    public GameFrame() throws HeadlessException {
+            this.add(new GamePanel());
+            this.setTitle("Snake");
+            this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            this.setResizable(false);
+            this.pack();
+            this.setVisible(true);
+            this.setLocationRelativeTo(null);
+
+    }
+}
---
+++ src/game/SnakeGame.java
@@ -0,0 +1,11 @@
+package game;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SnakeGame {
+    public static void main(String[] args) {
+        new GameFrame();
+    }
+}
+
